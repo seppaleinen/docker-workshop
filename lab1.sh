437193dd6bf996aaf29ed5d3cda34fae23bad13c
David Eriksson <davideriksson@swedenmail.com>
2016-12-07 18:14:23 +0100

    lab1 fix

--- lab1.sh
+++ lab1.sh
@@ -1,30 +1,33 @@
 #!/usr/bin/env bash
 
+echo 'STEP 1';
 docker images | grep -E '^nginx';
 
+echo 'STEP 2';
 docker run -p 8080:80 -d nginx && curl localhost:8080 | grep '<title>Welcome to nginx!</title>' && docker ps -aq | xargs docker rm -f;
 
+echo 'STEP 3';
 docker run -p 8080:80 -d nginx &&
 docker run -p 8080:80 -d nginx | grep 'port is already allocated' &&
 docker ps -aq | xargs docker rm -f;
 
-
+echo 'STEP 4';
 docker images;
 
-
+echo 'STEP 5';
 docker run -e HEJ=HOJ -d nginx &&
 docker ps -aq | xargs docker inspect | grep 'HEJ' &&
 docker ps -aq | xargs docker rm -f;
 
-
+echo 'STEP 6';
 docker run -d nginx &&
-NGINX_PID=$( docker ps -aq );
-docker exec -ti "$NGINX_PID" env | grep 'HEJ' &&
-docker exec -ti "$NGINX_PID" ps -ef &&
-docker exec -ti "$NGINX_PID" ps -ef | grep -i 'nginx' | awk '{print $0}' | xargs kill -9 &&
+docker exec -ti $( docker ps -aq ) env | grep 'HEJ' &&
+docker exec -ti $( docker ps -aq ) ps -ef &&
+docker exec -ti $( docker ps -aq ) ps -ef | grep -i 'nginx' | awk '{print $0}' | xargs kill -9 &&
 docker ps -aq | xargs docker rm -f;
 
-
+echo "STEP 7";
 docker ps -a;
 
+echo "STEP 8";
 docker ps -aq | xargs docker rm -f;
