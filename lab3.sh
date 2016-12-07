7ef065df4c271ae229d6f1832ab760902ce13245
David Eriksson <davideriksson@swedenmail.com>
2016-12-07 18:58:06 +0100

    Lab3

--- lab3.sh
+++ lab3.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/env bash
+
+echo "STEP 1";
+docker build --rm -t static_web_server static_web_server;
+
+docker run -p 8080:80 -d static_web_server;
+
+curl localhost:8080;
+
+docker ps -aq | xargs docker rm -f;
