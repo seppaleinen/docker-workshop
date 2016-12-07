2a5793f054f17a5ee653a2f9773e1dfd8f228ce8
David Eriksson <davideriksson@swedenmail.com>
2016-12-07 19:41:37 +0100

    Lab4

--- lab4.sh
+++ lab4.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/env bash
+
+./gradlew -b db-service/build.gradle clean build;
+
+docker-compose -f db-service/docker-compose.yml up -d --build;
+
+sleep 5;
+curl http://localhost:8080/person/list;
+
+docker-compose -f db-service/docker-compose.yml down;
