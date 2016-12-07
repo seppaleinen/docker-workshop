9185487efff6ccdeb24f10bc6faa3db54bbad97f
David Eriksson <davideriksson@swedenmail.com>
2016-12-07 18:35:22 +0100

    Lab2

--- lab2.sh
+++ lab2.sh
@@ -1,1 +1,8 @@
 #!/usr/bin/env bash
+
+echo "STEP 1";
+docker build --rm -t silly_server silly_server;
+
+docker run silly_server;
+
+docker ps -aq | xargs docker rm -f;
--- silly_server/silly.sh
+++ silly_server/silly.sh
@@ -1,8 +1,3 @@
 #! /bin/bash
 
-COUNTER=1
-while [ true ]; do
-  cowsay "Rocking with Docker!!! ${COUNTER}"
-  let COUNTER=COUNTER+1
-  sleep 5
-done
+/usr/games/cowsay "Rocking with Docker!!! ${COUNTER}"
